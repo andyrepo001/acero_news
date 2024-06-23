9d882c57380c1fb2215d935884e1283fe80b0051
tayef <tayef@mail.xyz>
2024-06-23 17:12:09 +0600

    meta

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,9 @@
 import { redirect } from "next/navigation";
 
+export const metadata = {
+  title: "Home | Acero News",
+};
+
 export default function Home({ params }) {
   if (params !== "bn" || "en") redirect("/bn");
   return null;

c2667a005b8ca8dc987bb0e899d7f868e8b8fbca
tayef <tayef@mail.xyz>
2024-06-23 16:18:09 +0600

    image hostname

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "newsaccess.anvildynamics.xyz",
+        hostname: "news.anvildynamics.xyz",
       },
     ],
   },

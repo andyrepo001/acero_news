6911dc71bc3ecf951bc0c26b943ca86ae4298d1c
tayef <tayef@mail.xyz>
2024-06-23 15:48:00 +0600

    env

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    SERVER_API_URL: "http://api.localaceronews.com/",
+    SERVER_API_URL: "https://newsaccess.anvildynamics.xyz/",
     SERVER_API_KEY: "MzgzXzE3MTczMzE4MzU2NDdfMzQw",
   },
   images: {

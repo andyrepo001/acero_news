689cac0b3af7555d6c856804a40f5a3ed40b8513
tayef <tayef@mail.xyz>
2024-06-24 15:47:30 +0600

    router issue

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,23 +1,10 @@
-"use client";
 import "./globals.css";
 import Footer from "@/components/footer/footer";
 import Navbar from "@/components/navbar/navbar";
-import { useParams, useRouter } from "next/navigation";
-import { useEffect } from "react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
 
 export default function RootLayout({ children }) {
-  const params = useParams();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (params?.lan !== "bn" && params?.lan !== "en") {
-      router.back();
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [params?.lan]);
-
   return (
     <html lang="en">
       <body>

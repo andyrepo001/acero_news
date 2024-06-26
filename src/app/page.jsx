1ade2e26a8acabbe9cb106cd51e7ba0369db8f92
tayef <tayef@mail.xyz>
2024-06-26 14:32:16 +0600

    middleware redirection

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,7 @@
-import { redirect } from "next/navigation";
-
 export const metadata = {
   title: "Home | Acero News",
 };
 
-export default function Home({ params }) {
-  if (params !== "bn" || "en") redirect("/bn");
+export default function Home() {
   return null;
 }
